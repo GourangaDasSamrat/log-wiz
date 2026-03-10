0602d3e9d2d04ae56a3fab944b922610199093a7
GourangaDasSamrat <gouranga.samrat@gmail.com>
2026-03-10 11:45:00 +0000

    chore(tooling): configure ESLint, Prettier and Jest
    - ESLint extends @typescript-eslint/recommended with type-checked rules
    - Prettier enforces single quotes, trailing commas and 100-char line width
    - Jest uses ts-jest preset with moduleNameMapper to strip .js extensions

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-console': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'jest.config.cjs', '*.cjs'],
+};
---
+++ jest.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      useESM: false,
+    }],
+  },
+  moduleNameMapper: {
+    // Strip .js extensions for CJS Jest resolution
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 75,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
